0dada30d1cf5dd837ae54d17cfc811098b7b5ba5
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-09-18 01:50:17 +0900

    Initial Commit

---
+++ 2023BIFF/_023BIFFApp.swift
@@ -0,0 +1,20 @@
+//
+//  _023BIFFApp.swift
+//  2023BIFF
+//
+//  Created by GYURI PARK on 2023/09/18.
+//
+
+import SwiftUI
+
+@main
+struct _023BIFFApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
